219fd47bfa7b1187d6842d730c7f1fadbfdc4644
Karim <KariimAhmed8@gmail.com>
2021-01-05 00:51:31 +0200

    #Update Constant file

--- lib/constants.dart
+++ lib/constants.dart
@@ -7,4 +7,59 @@ class Constants {
   static var rayDomain = 'raywenderlich.com';
   static var rayLogo = 'images/raylogo.png';
   static var appTitle = 'Raywenderlich E-Book';
+  static var snackBarText = 'URL Copied to Clipboard';
+  static var xcodeSimulatorHeader =
+      "The Xcode Simulator is one of the tools used most widely by developers:";
+  static var xcodeSimulatorBody =
+      "Running and testing apps on the simulator has become part of every developer’s daily routine."
+      " Becoming familiar with various simulator options is vital for any developer. Did you know"
+      " you can create and configure simulators from the command line as well? In this tutorial, you’ll learn:"
+      "What a simulator is"
+      "Insights into useful simulator options"
+      "To create and configure simulators from the command line"
+      "To stream and capture logs using the command line"
+      "To create a Bash script to automate launching the app on a simulator in different locales."
+      "Getting Started"
+      "Download the project by clicking the Download Materials button at the top or bottom of this page."
+      " Open the RayWonders project. Build and run.  ";
+  static var xcodeSimulatorImage = "images/ray1.png";
+  static var pixelHeader = "Pixel Accurate";
+  static var pixelBody =
+      "In this mode, the window size changes to the same number"
+      " of pixels as the physical device. Each pixel on the simulated device maps"
+      " to one pixel on your Mac’s display. This causes simulators to appear"
+      " larger on screen if your Mac’s display has a lower pixel density than "
+      "the simulated device. You can use this mode to check the alignment of images"
+      " and controls in your app.";
+  static var pixelImage = "images/ray2.png";
+  static var animationHeader = "Slow Animations";
+  static var animationBody =
+      "Animations are an integral part of an app experience."
+      " Build and run RayWonders in the Demo simulator."
+      "Tap the Photos tab. Next, tap a picture of one of the wonders of"
+      "the world to present a view of details about the place. Dismiss the view by sliding it down."
+      "To simulate slow animations, select the Debug ▸ Slow Animations option in the Simulator menu.";
+  static var animationImage = "images/ray3.png";
+  static var darkModeHeader = "Dark Mode";
+  static var darkModeBody =
+      "provides a great viewing experience in low-light environments. "
+      "The simulator provides an option to view your app in Dark Mode."
+      "Select Features ▸ Toggle Appearance. This toggles the appearance "
+      "to Dark Mode. Now, tap the Map tab in RayWonders.You’ll notice that"
+      " the map has changed to a dark appearance. Easy, isn’t it? This is "
+      "a handy way to test your app in Dark Mode."
+      " To change the appearance back to default, deselect Features ▸ Toggle appearance."
+      " To learn how to support Dark Mode";
+  static var darkModeImage = "images/ray4.png";
+  static var sharingLocationHeader = "Sharing Locations From the Maps App";
+  static var sharingLocationBody =
+      "The Maps app on macOS provides an easy way to share locations "
+      "with your simulator."
+      "Follow these steps:"
+      " Open the Maps app."
+      " Enter Machu Picchu in the search text."
+      " Click the Share button next to the search field."
+      "Choose Simulator from the drop-down."
+      "In the location prompt, select Demo as the simulator.";
+  static var sharingLocationImage = "images/ray5.png";
 }
