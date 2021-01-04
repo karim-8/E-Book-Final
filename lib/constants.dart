720fcda71a94f40221e0314f626a5cd7c0a326ca
Karim <KariimAhmed8@gmail.com>
2021-01-05 00:23:55 +0200

    #Attatching constants file

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+class Constants {
+  static var rayUrl =
+      'https://www.raywenderlich.com/16971076-xcode-simulator-app-advanced#toc-anchor-002';
+  static var copyButton = 'Copy Url';
+  static var articleUrl =
+      'https://www.raywenderlich.com/16971076-xcode-simulator-app-advanced#toc-anchor-002';
+  static var rayDomain = 'raywenderlich.com';
+  static var rayLogo = 'images/raylogo.png';
+  static var appTitle = 'Raywenderlich E-Book';
+}
