c8cfdf5133ad3eff288016ad869dbe0fb70500fa
Karim <kariimAhmed8@gmail.com>
2022-04-28 20:01:46 +0200

    update assets

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,51 +1,12 @@
-/*
-Copyright (c) 2021 Razeware LLC
-
-Permission is hereby granted, free of charge, to any person
-obtaining a copy of this software and associated documentation
-files (the "Software"), to deal in the Software without
-restriction, including without limitation the rights to use,
-copy, modify, merge, publish, distribute, sublicense, and/or
-sell copies of the Software, and to permit persons to whom
-the Software is furnished to do so, subject to the following
-conditions:
-
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
-
-Notwithstanding the foregoing, you may not use, copy, modify,
-merge, publish, distribute, sublicense, create a derivative work,
-and/or sell copies of the Software in any work that is designed,
-intended, or marketed for pedagogical or instructional purposes
-related to programming, coding, application development, or
-information technology. Permission for such use, copying,
-modification, merger, publication, distribution, sublicensing,
-creation of derivative works, or sale is expressly withheld.
-
-This project and source code may use libraries or frameworks
-that are released under various Open-Source licenses. Use of
-those libraries and frameworks are governed by their own
-individual licenses.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-DEALINGS IN THE SOFTWARE.
- */
-
 class Constants {
   static var rayUrl =
-      'https://www.raywenderlich.com/16971076-xcode-simulator-app-advanced#toc-anchor-002';
+      'https://medium.com/@Karim_Soliman';
   static var copyButton = 'Copy Url';
   static var articleUrl =
-      'https://www.raywenderlich.com/16971076-xcode-simulator-app-advanced#toc-anchor-002';
-  static var rayDomain = 'raywenderlich.com';
-  static var rayLogo = 'images/raylogo.png';
-  static var appTitle = 'raywenderlich E-Book';
+      'https://medium.com/@Karim_Soliman';
+  static var rayDomain = 'Medium.com';
+  static var rayLogo = 'images/medium.png';
+  static var appTitle = 'Medium E-Book';
   static var snackBarText = 'URL Copied to Clipboard';
   static var xcodeSimulatorHeader =
       "The Xcode Simulator is one of the tools used most widely by developers:";
@@ -97,7 +58,7 @@ class Constants {
       "In the location prompt, select Demo as the simulator.";
   static var sharingLocationImage = "images/ray5.png";
   static var visit = "Vist website";
-  static var mainUrl = "https://www.raywenderlich.com/whats-new";
+  static var mainUrl = "https://medium.com/@Karim_Soliman";
   static var copyMessage = "Article URL Copied to Clipboard";
-  static var visitMessage = "raywenderlich URL Copied to Clipboard";
+  static var visitMessage = " URL Copied to Clipboard";
 }
